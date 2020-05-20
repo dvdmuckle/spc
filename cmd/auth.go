14ea850a2c13c9f52611ed35871f010d65dfa050
David Muckle <dvdmuckle@dvdmuckle.xyz>
2020-05-19 22:27:33 -0400

    Write to the specific config file

--- cmd/auth.go
+++ cmd/auth.go
@@ -67,7 +67,7 @@ func auth() {
 			glog.Fatal(err)
 		}
 		viper.Set("auth", token)
-		viper.WriteConfig()
+		viper.WriteConfigAs(cfgFile)
 		fmt.Println("Login successful as ", user.ID)
 	}
 }
@@ -91,13 +91,9 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 // authCmd represents the auth command
 var authCmd = &cobra.Command{
 	Use:   "auth",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Authenticates with Spotify",
+	Long: `Authenticates with Spotify by printout out a login link, which will then save your access token to the config file
+	       Use this command after the initial login to refresh your access token`,
 	Run: func(cmd *cobra.Command, args []string) {
 		auth()
 	},
@@ -105,14 +101,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(authCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// authCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// authCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
