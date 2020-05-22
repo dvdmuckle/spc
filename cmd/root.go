6d9932af55a7d56a753e7581cb6911082b568e72
David Muckle <dvdmuckle@dvdmuckle.xyz>
2020-05-22 18:24:09 -0400

    Change root command help

--- cmd/root.go
+++ cmd/root.go
@@ -39,16 +39,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "goify",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Command line tool to control Spotify",
+	Long: `Goify is a simple command line tool to control Spotify using the Spotify API
+to allow for cross platform use. It is designed to be simple and is limited in
+scope, and is best when paired with another more complicated tool.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
