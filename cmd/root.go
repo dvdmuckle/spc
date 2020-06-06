9e71e6efc30e3d959ced3fef0d7a368812809073
David Muckle <dvdmuckle@dvdmuckle.xyz>
2020-06-06 17:24:42 -0400

    Custom format for status, adjust root to not print config used

--- cmd/root.go
+++ cmd/root.go
@@ -88,8 +88,8 @@ func initConfig() {
 	viper.SetConfigType("yaml")
 	viper.AutomaticEnv() // read in environment variables that match
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		glog.Fatal("Error reading config file:", err)
 	}
 	if err := viper.WriteConfigAs(cfgFile); err != nil {
 		glog.Fatal("Error writing config file:", err)
--- cmd/status.go
+++ cmd/status.go
@@ -16,19 +16,64 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/golang/glog"
 	"github.com/spf13/cobra"
+	"github.com/zmb3/spotify"
 )
 
-// statusCmd represents the status command
+type Status spotify.CurrentlyPlaying
+
 var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Get the currently playing song from Spotify",
-	Long:  ``,
+	Long: `Get the currently playing song from Spotify
+
+	A format string can be passed with --format or -f to specify what
+	the status printout should look like. The following fields are available:
+
+	%a - artist
+	%t - track
+	%b - album
+	%p - playing`,
 	Run: func(cmd *cobra.Command, args []string) {
+		status, err := conf.Client.PlayerCurrentlyPlaying()
+		if err != nil {
+			glog.Fatal(err)
+		}
+		fmt.Printf("%s - %s\n", status.Item.SimpleTrack.Artists[0].Name, status.Item.SimpleTrack.Name)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(statusCmd)
 	statusCmd.Flags().StringP("format", "f", "", "Format string for formatting the status")
+}
+
+//Format implements Formatter for Spotify status
+func (stat Status) Format(state fmt.State, verb rune) {
+	switch verb {
+	case 'b':
+		fmt.Fprint(state, stat.Item.Album.Name)
+	case 't':
+		fmt.Fprint(state, stat.Item.SimpleTrack.Name)
+	case 'p':
+		fmt.Fprint(state, stat.Playing)
+	case 'a':
+		wid, set := state.Width()
+		if set {
+			for i, artist := range stat.Item.SimpleTrack.Artists {
+				//If we are printing the last artist, we don't want a comma
+				if i == wid || i == len(stat.Item.SimpleTrack.Artists) {
+					fmt.Fprint(state, artist.Name)
+					continue
+				} else {
+					fmt.Fprint(state, artist.Name+", ")
+				}
+			}
+		} else {
+			fmt.Fprint(state, stat.Item.SimpleTrack.Artists[0].Name)
+		}
+	}
 }
