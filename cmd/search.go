1a75e00fea9a13d1d0dfa3298f548ad51bb1ba99
David Muckle <dvdmuckle@dvdmuckle.xyz>
2020-07-19 22:41:22 -0400

    Working search

--- cmd/search.go
+++ cmd/search.go
@@ -30,41 +30,105 @@ import (
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
 	Use:   "search",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Seach for and play a track, album, or playlist",
+	Long: `Seach takes two arguments: the search type, and the query.
+Search type can be either track or album, with the rest of the arguments
+making up the search query. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	goify search album moving pictures
+	goify search track tom sawyer
+	goify search playlist happy vibes
+
+More advanced options are availble for the search query. For this,
+please see https://pkg.go.dev/github.com/zmb3/spotify?tab=doc#Client.Search`,
 	Run: func(cmd *cobra.Command, args []string) {
 		helper.SetClient(&conf)
-		searchTerm := strings.Join(args, " ")
-		searchResults, err := conf.Client.Search(searchTerm, spotify.SearchTypeTrack)
+		var searchType string
+		if len(args) < 2 {
+			fmt.Println("Please search for a track, album, or playlist")
+			os.Exit(1)
+		}
+		if args[0] == "track" || args[0] == "album" || args[0] == "playlist" {
+			searchType = args[0]
+		} else {
+			fmt.Println("Please search for a track, album, or playlist")
+			os.Exit(1)
+		}
+		searchTerm := strings.Join(args[1:], " ")
+		var searchResults *spotify.SearchResult
+		var err error
+		switch searchType {
+		case "track":
+			searchResults, err = conf.Client.Search(searchTerm, spotify.SearchTypeTrack)
+		case "album":
+			searchResults, err = conf.Client.Search(searchTerm, spotify.SearchTypeAlbum)
+		case "playlist":
+			searchResults, err = conf.Client.Search(searchTerm, spotify.SearchTypePlaylist)
+		}
 		if err != nil {
 			glog.Fatal(err)
 		}
-		toPlay := fuzzySearchResults(*searchResults)
-		conf.Client.QueueSong(toPlay)
+		toPlay := fuzzySearchResults(*searchResults, searchType)
+		var opts spotify.PlayOptions
+		opts.DeviceID = &conf.DeviceID
+		switch searchType {
+		case "track":
+			opts.URIs = append(opts.URIs, toPlay)
+		case "album", "playlist":
+			opts.PlaybackContext = &toPlay
+		}
+		if err := conf.Client.PlayOpt(&opts); err != nil {
+			glog.Fatal(err)
+		}
 	},
 }
 
-func fuzzySearchResults(results spotify.SearchResult) spotify.ID {
-	idx, err := fuzzyfinder.Find(
-		results.Tracks.Tracks,
-		func(i int) string {
-			return fmt.Sprintf("%s - %s", results.Tracks.Tracks[i].Artists[0].Name, results.Tracks.Tracks[i].Name)
-		})
-
+func fuzzySearchResults(results spotify.SearchResult, searchType string) spotify.URI {
+	var idx int
+	var err error
+	switch searchType {
+	case "track":
+		idx, err = fuzzyfinder.Find(
+			results.Tracks.Tracks,
+			func(i int) string {
+				return fmt.Sprintf("%s - %s - %s", results.Tracks.Tracks[i].Artists[0].Name,
+					results.Tracks.Tracks[i].Name,
+					results.Tracks.Tracks[i].Album.Name)
+			})
+	case "album":
+		idx, err = fuzzyfinder.Find(
+			results.Albums.Albums,
+			func(i int) string {
+				return fmt.Sprintf("%s - %s", results.Albums.Albums[i].Name,
+					results.Albums.Albums[i].Artists[0].Name)
+			})
+	case "playlist":
+		idx, err = fuzzyfinder.Find(
+			results.Playlists.Playlists,
+			func(i int) string {
+				return fmt.Sprintf("%s - %s", results.Playlists.Playlists[i].Name,
+					results.Playlists.Playlists[i].Owner.DisplayName)
+			})
+	}
 	if err != nil {
 		if err.Error() == "abort" {
-			fmt.Println("Aborted switch")
+			fmt.Println("Aborted search")
 			os.Exit(0)
 		}
 		glog.Fatal(err)
 	}
-	fmt.Println(results.Tracks.Tracks[idx].Name)
-	return results.Tracks.Tracks[idx].ID
+	switch searchType {
+	case "track":
+		return results.Tracks.Tracks[idx].URI
+	case "album":
+		return results.Albums.Albums[idx].URI
+	case "playlist":
+		return results.Playlists.Playlists[idx].URI
+	}
+	//The code should never get here because of our check of
+	//search types earlier, this is just to make the compiler
+	//happy
+	return ""
 }
 
 func init() {
