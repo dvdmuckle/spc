4fb68b8f2a6448025b3e2bfab1a58b2d8bd97b54
David Muckle <dvdmuckle@dvdmuckle.xyz>
2021-08-22 20:22:00 -0400

    Save discover weekly command thus far; currently panicing

---
+++ cmd/saveWeekly.go
@@ -0,0 +1,86 @@
+/*
+Copyright © 2021 David Muckle
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	"github.com/zmb3/spotify"
+)
+
+// saveWeeklyCmd represents the saveWeekly command
+var saveWeeklyCmd = &cobra.Command{
+	Use:   "save-weekly",
+	Short: "Saves the current Spotify Discover Weekly playlist",
+	Long: `Saves the current Spotify Discover Weekly playlist.
+	 Note this cannot bring back old Spotify Discover Weekly playlists, it can
+	  only save the current playlist`,
+	Run: func(cmd *cobra.Command, args []string) {
+		playlistName, _ := cmd.Flags().GetString("name")
+		playlistDescription := "Spotify Discover Weekly for " + getPlaylistDate()
+		if playlistName == "" {
+			playlistName = "Discover Weekly " + getPlaylistDate()
+		}
+		isPublic, _ := cmd.Flags().GetBool("public")
+		fmt.Println("Getting current user")
+		currentUser, err := conf.Client.CurrentUser()
+		fmt.Println("Getting current user")
+		if err != nil {
+			glog.Fatal(err)
+		}
+		fmt.Println("Making playlist")
+		newPlaylist, err := conf.Client.CreatePlaylistForUser(currentUser.User.ID, playlistName, playlistDescription, isPublic)
+		if err != nil {
+			glog.Fatal(err)
+		}
+		fmt.Println("Searching")
+		searchResult, err := conf.Client.Search("Discover Weekly", spotify.SearchTypePlaylist)
+		if err != nil {
+			glog.Fatal(err)
+		}
+		discoverPlaylistTracks := func(spotify.ID) spotify.PlaylistTrackPage {
+			fmt.Println("Getting playlist tracks")
+			playlistTracks, err := conf.Client.GetPlaylistTracks(searchResult.Playlists.Playlists[0].ID)
+			if err != nil {
+				glog.Fatal(err)
+			}
+			return *playlistTracks
+		}
+		var discoverPlaylistTrackIDs []spotify.ID
+		for _, track := range discoverPlaylistTracks(searchResult.Playlists.Playlists[0].ID).Tracks {
+			discoverPlaylistTrackIDs = append(discoverPlaylistTrackIDs, track.Track.ID)
+		}
+		conf.Client.AddTracksToPlaylist(newPlaylist.ID, discoverPlaylistTrackIDs...)
+
+	},
+}
+
+func getPlaylistDate() string {
+	date := time.Now()
+	for date.Weekday() != time.Monday {
+		date = date.AddDate(0, 0, -1)
+	}
+	return fmt.Sprintf("%d/%d/%d", date.Month(), date.Day(), date.Year())
+}
+
+func init() {
+	rootCmd.AddCommand(saveWeeklyCmd)
+	saveWeeklyCmd.Flags().StringP("name", "n", "", "Custom name for the save playlist")
+	saveWeeklyCmd.Flags().BoolP("public", "p", false, "Whether to make the new playlist public")
+}
