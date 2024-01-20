ec3c7317ff55ac07990ca866215c01ab6e7b71ca
David Muckle <dvdmuckle@dvdmuckle.xyz>
2024-01-20 15:05:17 -0500

    Update copyright, switch to maintained command for getting track list… (#184)

--- cmd/volume.go
+++ cmd/volume.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 David Muckle <dvdmuckle@dvdmuckle.xyz>
+Copyright © 2024 David Muckle <dvdmuckle@dvdmuckle.xyz>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
