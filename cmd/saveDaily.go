86df6044d0bc80a8b4ea17fa480b205fc97117b8
PHAGUN-JAIN <phagun.jain@protonmail.com>
2024-10-14 00:26:21 +0530

    updated save-daylist cmd to save with name given by spotify

--- cmd/saveDaily.go
+++ cmd/saveDaily.go
@@ -3,8 +3,6 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"time"
-
 	"github.com/dvdmuckle/spc/cmd/helper"
 	"github.com/spf13/cobra"
 	"github.com/zmb3/spotify/v2"
@@ -19,10 +17,6 @@ Note this saves the Daylist at the current time of day and cannot retrieve past 
 	Run: func(cmd *cobra.Command, args []string) {
 		helper.SetClient(&conf)
 		playlistName, _ := cmd.Flags().GetString("name")
-		playlistDescription := "Spotify Daylist for " + getDaylistDateTime()
-		if playlistName == "" {
-			playlistName = "Daylist " + getDaylistDateTime()
-		}
 		isPublic, _ := cmd.Flags().GetBool("public")
 		isCollaborative, _ := cmd.Flags().GetBool("collaborative")
 
@@ -31,32 +25,43 @@ Note this saves the Daylist at the current time of day and cannot retrieve past 
 		if err != nil {
 			helper.LogErrorAndExit(err)
 		}
-		if deduplicatePlaylist(playlistName, currentUser.User.ID) {
-			fmt.Println("Daylist already saved")
-			return
-		}
-		newPlaylist, err := conf.Client.CreatePlaylistForUser(ctx, currentUser.User.ID, playlistName, playlistDescription, isPublic, isCollaborative)
-		if err != nil {
-			helper.LogErrorAndExit(err)
-		}
+
 		searchResult, err := conf.Client.Search(ctx, "Daylist", spotify.SearchTypePlaylist)
 		if err != nil {
 			helper.LogErrorAndExit(err)
 		}
-		var daylistPlaylist spotify.ID
+
+		var daylistPlaylist spotify.SimplePlaylist
 		for _, playlist := range searchResult.Playlists.Playlists {
 			if playlist.Owner.ID == "spotify" {
-				daylistPlaylist = playlist.ID
+				daylistPlaylist = playlist
 				break
 			}
 		}
+
+		playlistDescription := "Spotify Daylist: " + daylistPlaylist.Name
+		if playlistName == "" {
+			playlistName = daylistPlaylist.Name
+		}
+
+		if deduplicatePlaylist(playlistName, currentUser.User.ID) {
+			fmt.Println("Daylist already saved")
+			return
+		}
+
+		newPlaylist, err := conf.Client.CreatePlaylistForUser(ctx, currentUser.User.ID, playlistName, playlistDescription, isPublic, isCollaborative)
+		if err != nil {
+			helper.LogErrorAndExit(err)
+		}
+
 		daylistTracks := func() spotify.PlaylistItemPage {
-			playlistTracks, err := conf.Client.GetPlaylistItems(ctx, daylistPlaylist)
+			playlistTracks, err := conf.Client.GetPlaylistItems(ctx, daylistPlaylist.ID)
 			if err != nil {
 				helper.LogErrorAndExit(err)
 			}
 			return *playlistTracks
 		}
+
 		var daylistTrackIDs []spotify.ID
 		for _, track := range daylistTracks().Items {
 			daylistTrackIDs = append(daylistTrackIDs, track.Track.Track.ID)
@@ -66,14 +71,9 @@ Note this saves the Daylist at the current time of day and cannot retrieve past 
 			helper.LogErrorAndExit(err)
 			return
 		}
+
 		fmt.Printf("Daylist saved as %s\n", playlistName)
 	},
-}
-
-func getDaylistDateTime() string {
-	// Gets the current date and time (hour) to differentiate Daylists
-	now := time.Now()
-	return fmt.Sprintf("%s %02d:%02d", now.Weekday(), now.Hour(), now.Minute())
 }
 
 func init() {
